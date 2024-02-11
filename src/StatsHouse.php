22e2a24c8f6399f431757259dd2c40101297e4b2
hrissan <g.buteyko@corp.vk.com>
2024-02-11 18:33:48 +0300

    Update StatsHouse.php
    Lex encoding of floats must be here.
    
    Signed-off-by: hrissan <g.buteyko@corp.vk.com>

--- src/StatsHouse.php
+++ src/StatsHouse.php
@@ -154,6 +154,41 @@ class StatsHouse {
     }
     return $this->maybeFlush($metric, $now);
   }
+  
+  /**
+   * Advanced feature.
+   * Encodes float as a raw tag in a special format, used by Statshouse.
+   * Ordering of most float values is preserved after encoding.
+   */
+  public static function lexEncFloat(float $f): int {
+    if (is_nan($f)) {
+      return 0; // no sortable binary representation for NaN
+    }
+    if ($f == 0) {
+      return 0;
+    }
+    $data = pack('f', $f);
+    $arr = unpack('l', $data);
+    if ($arr === false) {
+      return 0; // never
+    }
+    $l = (int)$arr[1];
+    if ($l < 0) {
+      $l ^= 0x7fffffff; // flip all except signbit so bigger negatives go before smaller ones
+    }
+    return $l;
+  }
+  public static function lexDecFloat(int $l): float {
+    if ($l < 0) {
+      $l ^= 0x7fffffff;
+    }
+    $data = pack('l', $l);
+    $arr = unpack('f', $data);
+    if ($arr === false) {
+      return 0; // never
+    }
+    return (float)$arr[1];
+  }
 
   /**
    * All integer array indexes will be replaced by 'key'.($k+1)
